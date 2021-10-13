de314533c012f4452e275b0ae189a31cf5c116bc
Paulo Salvatore <salvatoregames@gmail.com>
2021-10-13 13:21:23 -0300

    add port to listen message callback

--- index.js
+++ index.js
@@ -8,5 +8,5 @@ app.get("/", function (req, res) {
 });
 
 app.listen(port, () => {
-    console.info("Aplicação rodando em http://localhost:3000");
+    console.info("Aplicação rodando em http://localhost:" + port);
 });
